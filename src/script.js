c604713cb8aeb162b8af38d3647e31508a389473
lufasano <66181095+lufasano@users.noreply.github.com>
2020-07-12 11:54:41 +0100

    Created JS file

---
+++ src/script.js
@@ -0,0 +1,47 @@
+let now = new Date();
+let dateParagraph = document.querySelector("h3");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+dateParagraph.innerHTML = `${day} ${hour}:${minutes}`;
+
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temp");
+  let temperature = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = `${temperature}`;
+  let city = document.querySelector("#form-input").value;
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${city}`;
+}
+
+function searchCity(city) {
+  let key = "60165e6fa483c2ba25ca27f92df423ed";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(url).then(showTemperature);
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#form-input").value;
+  searchCity(city);
+}
+
+let form = document.querySelector("#my-form");
+form.addEventListener("submit", search);
